f908cc8051c1a57a7ea569ae0ac599e0659708b2
lu1scanel0 <163206131+lu1scanel0@users.noreply.github.com>
2024-05-26 18:08:24 -0400

    Delete Unidad02/EjercicioAngular23-05 directory

--- Unidad02/EjercicioAngular23-05/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListaProductosComponent } from './lista-productos/lista-productos.component';
-
-  export const routes: Routes = [
-    {path: 'productos', component: ListaProductosComponent},
-    {path: '', redirectTo: '/productos', pathMatch: 'full'}
-];
--- Unidad02/EjercicioAngular23-05/src/app/producto.ts
+++
@@ -1,8 +0,0 @@
-export interface Producto {
-  nombre: string;
-  precio: number;
-  color: string;
-  tamano: string;
-  tipo: string;
-  anoFabricacion: number;
-}
